5020206cc0cb1feba5cfd37be8e2f0886eb8a13e
LudovicaLuisaVissat <ludovicaluisavissat@c008147.wlan.net.ed.ac.uk>
2016-03-07 19:24:01 +0000

    Last version

--- Model/Simulator3.java
+++ Model/Simulator3.java
@@ -1,14 +1,15 @@
 package Model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+
 
 import Actions.Action;
 import Actions.EnvAction;
 import Actions.InfAction;
 import Actions.NoInfAction;
 import Actions.PassAction;
+import Utility.StdRandom;
 public class Simulator3 {
 
 	public static void main(String[] args) {
@@ -52,7 +53,7 @@ public class Simulator3 {
         //actions of agent S
 		Action a0 = new NoInfAction("birth", 0.4, "uparrow", "S");			
 		Action a1 = new NoInfAction("death", 0.3, "downarrow", "S");	
-		Action a2 = new PassAction("contact", 0.8, "dot", "S");			
+		Action a2 = new PassAction("contact", 0.9, "dot", "I");			
 		Action a3 = new NoInfAction("move", 0.2, "dot", "new");	
 		
 		ArrayList<Action> actionListS = new ArrayList<>();
@@ -62,13 +63,13 @@ public class Simulator3 {
 		addAction(a3, actionListS);
 		 	
 		//actions of agent I
-        Action a4 = new NoInfAction("birthI", 0.4, "uparrow", "S");		
-		Action a5 = new NoInfAction("deathI", 0.3, "downarrow", "S");			
-		Action a6 = new InfAction("contact", 0.8, "l", "dot", "S");			
+        //Action a4 = new NoInfAction("birthI", 0.4, "uparrow", "I");		
+		Action a5 = new NoInfAction("deathI", 0.4, "downarrow", "I");			
+		Action a6 = new InfAction("contact", 0.8, "l", "dot", "I");			
 		Action a7 = new NoInfAction("moveI", 0.2, "dot", "new");	
 		
 		ArrayList<Action> actionListI = new ArrayList<>();
-        addAction(a4, actionListI);
+       // addAction(a4, actionListI);
 		addAction(a5, actionListI);
 		addAction(a6, actionListI);
 		addAction(a7, actionListI);
@@ -86,14 +87,15 @@ public class Simulator3 {
         
         //setting time for the simulation
 		double time = 0;
-//		double timeTotal = 10;
+		double timeTotal = 10;
 				
-		//while(time < timeTotal){
+		while(time < timeTotal){
 		ArrayList<Double> PropFunc = new ArrayList<>();
 		ArrayList<Integer> agentArrayList = new ArrayList<>();
 		ArrayList<Integer> agentPositionArray = new ArrayList<>();
 		ArrayList<String> nameActions = new ArrayList<>();
 		ArrayList<Integer> passiveAgentPositionArray = new ArrayList<>();
+		ArrayList<Double> passiveProbArray = new ArrayList<>();
 		ArrayList<Integer> Type = new ArrayList<>();
 		
 		
@@ -109,6 +111,7 @@ public class Simulator3 {
 	 					       agentPositionArray.add(j);
 	 					       nameActions.add(action.getName());
 	 					       passiveAgentPositionArray.add(-1);//there is no passive agent
+	 					       passiveProbArray.add(0.0);
 	 					       Type.add(action.getType());
 	 				        }		        				
 	        				//inf action - check all the couple act-pass (depending on influence set)
@@ -126,8 +129,10 @@ public class Simulator3 {
 	 					 					    agentPositionArray.add(j);
 	 					 					    nameActions.add(action.getName());
 	 					 					    passiveAgentPositionArray.add(j);
-	 					 					  Type.add(action.getType());
-	 					 					  }}}}}}	 						      
+	 					 					    PassAction passAction = (PassAction) actionToCheck;	
+	 					 					    passiveProbArray.add(passAction.getInfProb());
+	 					 					    Type.add(action.getType());
+	 					 					    }}}}}}	 						      
 	 						     if (infaction.getInfSet() == "N(l)"){	 						    	 
 	 						    	 for(int k = 0; k < allActions.size(); k++){
 	 						    		 if(k != i){
@@ -141,6 +146,8 @@ public class Simulator3 {
 	 					 					    agentPositionArray.add(j);
 	 					 					    nameActions.add(action.getName());
 	 					 					    passiveAgentPositionArray.add(t);
+	 					 					    PassAction passAction = (PassAction) actionToCheck;	
+	 					 					    passiveProbArray.add(passAction.getInfProb());
 	 					 					    Type.add(action.getType());
 	 					 					    }}}}}}}	 						      
 	 						      if (infaction.getInfSet() == "all"){
@@ -156,6 +163,8 @@ public class Simulator3 {
 	 					 					    agentPositionArray.add(j);
 	 					 					    nameActions.add(action.getName());
 	 					 					    passiveAgentPositionArray.add(s);
+	 					 					    PassAction passAction = (PassAction) actionToCheck;	
+	 					 					    passiveProbArray.add(passAction.getInfProb());
 	 					 					    Type.add(action.getType());}}}}}}}}
 	 						    
 	 					   if (action.getType() == Action.ACTION_TYPE_Pass){
@@ -164,6 +173,7 @@ public class Simulator3 {
 				 			    agentPositionArray.add(j);
 				 			    nameActions.add(action.getName());
 							    passiveAgentPositionArray.add(j);
+							    passiveProbArray.add(0.0);
 							    Type.add(action.getType());}
 	 						      
 	        	
@@ -182,6 +192,8 @@ public class Simulator3 {
 		 					 					    agentPositionArray.add(-1);//env action no location
 		 					 					    nameActions.add(action.getName());
 		 					 					    passiveAgentPositionArray.add(s);
+		 					 					    PassAction passAction = (PassAction) actionToCheck;	
+		 					 					    passiveProbArray.add(passAction.getInfProb());
 		 					 					    Type.add(action.getType());}}}}}}}
 		 						  //the case of just one location (TO DO: more than one)
 		 						  else{int locName = Integer.parseInt(envaction.getInfSet());
@@ -194,15 +206,30 @@ public class Simulator3 {
 		 								  			agentArrayList.add(r);
 		 								  			agentPositionArray.add(position.get(locName));
 		 								  			nameActions.add(action.getName());
+		 					 					    PassAction passAction = (PassAction) actionToCheck;	
+		 					 					    passiveProbArray.add(passAction.getInfProb());
 		 								  			passiveAgentPositionArray.add(position.get(locName));}}}}}}}}}}
 		 						    		       				
-
-//		to check values of propensity function array
+	 		        				
 //		for(int i=0; i < PropFunc.size(); i++){ 
-//	    	    System.out.println(PropFunc.get(i));	    	    
-//	    }
-	 		        					        	   				
+//			System.out.println(PropFunc.get(i));
+//			System.out.println(agentArrayList.get(i));
+//			System.out.println(agentPositionArray.get(i));
+//			System.out.println(nameActions.get(i));
+//			System.out.println(passiveProbArray.get(i));
+//			System.out.println(passiveAgentPositionArray.get(i));
+//			System.out.println("end");
+//		}
+		
+		
 		double sumPropFunc = sum(PropFunc);
+		
+		if (sumPropFunc == 0){
+			System.out.println("End - null population");
+			break;
+		}else{
+		
+		
 		double[] Prob = new double[PropFunc.size()];
 
 		double time_passed = Samples.getExp(sumPropFunc);
@@ -216,7 +243,7 @@ public class Simulator3 {
 		for (int i = 0; i < PropFunc.size(); i++){
 			ActionRef[i] = i;
 		}	
-
+		
 		int action_index = Samples.getDiscrete(ActionRef, Prob);
 		
 		if(Type.get(action_index) == Action.ACTION_TYPE_NoInf){
@@ -239,21 +266,17 @@ public class Simulator3 {
 		if (symbol == "dot"){
 			if (chosenAction.getUpdate().matches("(?i).*new*")){				
 				//so far just random movement
-				int actualposition = agentPositionArray.get(action_index);
+				int actualposition = agentPositionArray.get(action_index);			
 				int[] neighbourhood = neighLocations[actualposition];
 				double[] ProbLoc = new double[neighbourhood.length];
-				for (int j=0; i < loc.length; j++){
-					if (Arrays.asList(neighbourhood).contains(loc[j])){
-						ProbLoc[j] = 1/neighbourhood.length;
-					}else{
-						ProbLoc[j] = 0;
-					}
-				}				
-				int new_l = Samples.getDiscrete(loc, ProbLoc);
-				populations[agentArrayList.get(action_index)][agentPositionArray.get(action_index)]--;					
-				populations[agentArrayList.get(action_index)][agentPositionArray.get(new_l)]++;	
-				System.out.println("Spatial action");
-				}else{
+				for (int j=0; j < neighbourhood.length; j++){				
+						ProbLoc[j] = (1.0/neighbourhood.length);
+				}
+				int new_l = Samples.getDiscrete(neighbourhood, ProbLoc);
+				int positionnew_l = position.get(new_l);
+				populations[agentArrayList.get(action_index)][actualposition]--;					
+				populations[agentArrayList.get(action_index)][positionnew_l]++;
+				System.out.println("Spatial action");}else{
                 //change of state
 				String newState = action.getUpdate();
 				
@@ -265,24 +288,166 @@ public class Simulator3 {
 				}
 				populations[agentArrayList.get(action_index)][agentPositionArray.get(action_index)]--;					
 				populations[newStatePos][agentPositionArray.get(action_index)]++;					
-				System.out.println("State action");
-			}
+				System.out.println("State action");}}}}}}
 		
-		}
 		
-		        	}
-		        }			
-		    }
-		}
+		
+		if(Type.get(action_index) == Action.ACTION_TYPE_Inf){
+			double probEffect = passiveProbArray.get(action_index);
+				if(StdRandom.bernoulli(probEffect)){
+					String nameToCheck = nameActions.get(action_index);
+						for(int i=0; i < AgentNames.size(); i++){
+					        for(Action action: AgentAction.get(AgentNames.get(i))){
+					        	if(action.getName() == nameToCheck && action.getType() == Action.ACTION_TYPE_Inf){
+					        		InfAction chosenAction = (InfAction) action;		        	
+					        		String symbolInf = chosenAction.getSymbol();
+				    //update for the influence agent
+					//demo+
+					if (symbolInf == "uparrow"){
+					populations[agentArrayList.get(action_index)][agentPositionArray.get(action_index)]++;	
+					System.out.println("Demographic+ action - Inf");
+					}
+					//demo-
+					if (symbolInf == "downarrow"){
+					populations[agentArrayList.get(action_index)][agentPositionArray.get(action_index)]--;					
+					System.out.println("Demographic- action - Inf");
+					}
+					if (symbolInf == "dot"){
+						if (chosenAction.getUpdate().matches("(?i).*new*")){				
+							//so far just random movement
+							int actualposition = agentPositionArray.get(action_index);			
+							int[] neighbourhood = neighLocations[actualposition];
+							double[] ProbLoc = new double[neighbourhood.length];
+							for (int j=0; j < neighbourhood.length; j++){				
+									ProbLoc[j] = (1.0/neighbourhood.length);
+							}
+							int new_l = Samples.getDiscrete(neighbourhood, ProbLoc);
+							int positionnew_l = position.get(new_l);
+							populations[agentArrayList.get(action_index)][actualposition]--;					
+							populations[agentArrayList.get(action_index)][positionnew_l]++;
+							System.out.println("Spatial action - Inf");}else{
+			                //change of state
+							String newState = action.getUpdate();
+							
+							int newStatePos = 0;				
+							for(int k=0; k < AgentNames.size(); k++){
+								if(newState == AgentNames.get(k)){
+									newStatePos = k;	
+								}
+							}
+							populations[agentArrayList.get(action_index)][agentPositionArray.get(action_index)]--;					
+							populations[newStatePos][agentPositionArray.get(action_index)]++;					
+							System.out.println("State action - Inf");}}
+										
+					//update of passive agents
+					for (int k=0; k < Names.length; k++){					
+					for(Action actionToCheckPass: AgentAction.get(AgentNames.get(k))){
+					        	if(actionToCheckPass.getName() == nameToCheck && actionToCheckPass.getType() == Action.ACTION_TYPE_Pass){
+					        		PassAction chosenActionPass = (PassAction) actionToCheckPass;		        	
+					        		String symbolPass = chosenActionPass.getSymbol();
+					        		int PassAgentName = k;
+					        		int PassAgentPosition = passiveAgentPositionArray.get(action_index);
+					    //demo+
+					    if (symbolPass == "uparrow"){
+						populations[PassAgentName][PassAgentPosition]++;	
+						System.out.println("Demographic+ action - Pass");
+						}
+						//demo-
+						if (symbolPass == "downarrow"){
+						populations[PassAgentName][PassAgentPosition]--;					
+						System.out.println("Demographic- action - Pass");
+						}
+						if (symbolPass == "dot"){
+							if (chosenActionPass.getUpdate().matches("(?i).*new*")){				
+								//so far just random movement			
+								int[] neighbourhood = neighLocations[PassAgentPosition];
+								double[] ProbLoc = new double[neighbourhood.length];
+								for (int j=0; j < neighbourhood.length; j++){				
+										ProbLoc[j] = (1.0/neighbourhood.length);
+								}
+								int new_l = Samples.getDiscrete(neighbourhood, ProbLoc);
+								int positionnew_l = position.get(new_l);
+								populations[PassAgentName][PassAgentPosition]--;					
+								populations[PassAgentName][positionnew_l]++;
+								System.out.println("Spatial action - Pass");}else{
+				                //change of state
+								String newState = action.getUpdate();
+								int newStatePos = 0;				
+								for(int q=0; q < AgentNames.size(); q++){
+									if(newState == AgentNames.get(q)){
+										newStatePos = q;	
+									}
+								}
+								populations[PassAgentName][PassAgentPosition]--;					
+								populations[newStatePos][PassAgentPosition]++;					
+								System.out.println("State action - Pass");}}}}}
+					
+					        			
+		
+		if(Type.get(action_index) == Action.ACTION_TYPE_Env){			
+			//update of passive agents (after an Env action)
+			for (int k=0; k < Names.length; k++){					
+			for(Action actionToCheckPass: AgentAction.get(AgentNames.get(k))){
+			        	if(actionToCheckPass.getName() == nameToCheck && actionToCheckPass.getType() == Action.ACTION_TYPE_Pass){
+			        		PassAction chosenActionPass = (PassAction) actionToCheckPass;		        	
+			        		String symbolPass = chosenActionPass.getSymbol();
+			        		int PassAgentName = k;
+			        		int PassAgentPosition = passiveAgentPositionArray.get(action_index);
+			    //demo+
+			    if (symbolPass == "uparrow"){
+				populations[PassAgentName][PassAgentPosition]++;	
+				System.out.println("Demographic+ action - Pass");
+				}
+				//demo-
+				if (symbolPass == "downarrow"){
+				populations[PassAgentName][PassAgentPosition]--;					
+				System.out.println("Demographic- action - Pass");
+				}
+				if (symbolPass == "dot"){
+					if (chosenActionPass.getUpdate().matches("(?i).*new*")){				
+						//so far just random movement			
+						int[] neighbourhood = neighLocations[PassAgentPosition];
+						double[] ProbLoc = new double[neighbourhood.length];
+						for (int j=0; j < neighbourhood.length; j++){				
+								ProbLoc[j] = (1.0/neighbourhood.length);
+						}
+						int new_l = Samples.getDiscrete(neighbourhood, ProbLoc);
+						int positionnew_l = position.get(new_l);
+						populations[PassAgentName][PassAgentPosition]--;					
+						populations[PassAgentName][positionnew_l]++;
+						System.out.println("Spatial action - Pass");}else{
+		                //change of state
+						String newState = action.getUpdate();
+						int newStatePos = 0;				
+						for(int q=0; q < AgentNames.size(); q++){
+							if(newState == AgentNames.get(q)){
+								newStatePos = q;	
+							}
+						}
+						populations[PassAgentName][PassAgentPosition]--;					
+						populations[newStatePos][PassAgentPosition]++;					
+						System.out.println("State action - Pass");}}}}}
+			
+			
+		}}}}}}
+			
+		
+		
 		time = time + time_passed;
 		for(int i=0; i < AgentNames.size(); i++){
 	        	for(int j=0; j < Locations.size(); j++){
-	        		System.out.println(populations[i][j]);
+	        		System.out.println(AgentNames.get(i) + "(" + loc[j] + ")=" + populations[i][j]);
 	        		}
 		}
+	        	System.out.println("Time: " + time);
+	        	System.out.println("-----");
+		}}}
 		
-	}	
+		
 	
+	
+	
+       //methods	
 	
 	    public static double sum (ArrayList<Double> list) {
 	       double sum = 0;
@@ -303,6 +468,9 @@ public class Simulator3 {
 		public static void addLoc(Integer loc, ArrayList<Integer> list){
 			list.add(loc);
 		}
+		
+		
+	  
 		
 }	
 				
